52199dfe1d1e626754276d229f0a09dc7c2f0094
Frese Fea <fresefae@gmail.com>
2026-03-25 20:32:48 +0300

    Попытка 7

---
+++ Popitka7/main.py
@@ -0,0 +1,170 @@
+import sqlite3
+from fastapi import FastAPI, Form, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import RedirectResponse
+from starlette.middleware.sessions import SessionMiddleware
+
+app = FastAPI()
+app.add_middleware(SessionMiddleware, secret_key="banana")
+
+templates = Jinja2Templates(directory="templates")
+
+def get_db():
+    conn = sqlite3.connect("database.db")
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def init_db():
+    with get_db() as conn:
+        conn.executescript("""
+    create table if not exists users(
+                           id integer primary key autoincrement,
+                           login text unique not null,
+                           password text not null,
+                           fio text not null,
+                           phone text not null,
+                           email text not null
+                           );
+
+    create table if not exists requests(
+                        id integer primary key autoincrement,
+                        user_id integer references users(id) on delete cascade,
+                        course_name text not null,
+                        date_start text not null,
+                        payment_method text not null,
+                        status text not null default 'Новая',
+                        review text
+                        );
+
+""")
+        
+init_db()
+
+@app.get("/")
+def get_base(request: Request):
+    return RedirectResponse(url="/login", status_code=302)
+
+@app.get("/logout")
+def get_logout(request: Request):
+    request.session.clear()
+    return RedirectResponse(url="/login", status_code=302)
+
+@app.get("/login")
+def get_login(request: Request):
+    return templates.TemplateResponse("login.html",{"request":request, "error": None})
+
+@app.get("/register")
+def get_register(request: Request):
+    return templates.TemplateResponse("register.html",{"request":request, "error": None})
+
+@app.post("/register")
+def post_register(request: Request,
+                  login: str = Form(...),
+                  password: str = Form(...),
+                  fio: str = Form(...),
+                  phone: str = Form(...),
+                  email: str = Form(...)):
+    
+    with get_db() as conn:
+        conn.execute("insert into users (login,password,fio,phone,email) values (?,?,?,?,?)", (login,password,fio,phone,email))
+    return RedirectResponse(url="/login", status_code=302)
+
+
+@app.post("/login")
+def post_login(request: Request,
+                  login: str = Form(...),
+                  password: str = Form(...)):
+    if login == "Admin":
+        if password == "KorokNET":
+            request.session["admin"]=  True
+            return RedirectResponse(url="/admin", status_code=302)
+        else:
+            return templates.TemplateResponse("login.html",{"request":request, "error": "Неверные данные"})
+         
+    with get_db() as conn:
+        user = conn.execute("select * from users where login = ?", (login,)).fetchone()
+        
+    if user and user["password"] == password:
+        request.session["user_id"] = user["id"]
+        return RedirectResponse(url="/profile", status_code=302)
+    return templates.TemplateResponse("login.html",{"request":request, "error": "Неверные данные"})
+   
+@app.get("/profile")
+def get_profile(request: Request):
+    user_id = request.session.get("user_id")
+    if not user_id:
+        return RedirectResponse(url="/login", status_code=302)
+
+    with get_db() as conn:
+        requests_list = conn.execute("select * from requests where user_id =  ?", (user_id,)).fetchall()
+    
+    return templates.TemplateResponse("profile.html",{"request":request, "requests": requests_list})
+
+@app.get("/admin")
+def get_admin(request: Request):
+    user_id = request.session.get("admin")
+    if not user_id:
+        return RedirectResponse(url="/login", status_code=302)
+
+    with get_db() as conn:
+        requests_list = conn.execute("select requests.*, users.login, users.fio from requests,users where requests.user_id = users.id").fetchall()
+    
+    return templates.TemplateResponse("admin.html",{"request":request, "requests": requests_list})
+
+
+COURSES = ["База данных", "Алгоритмы", "Роблокс"]
+PAYMNETS = ["Наличные","Перевод по номеру"]
+   
+@app.get("/create_request")
+def get_create_request(request: Request):
+    user_id = request.session.get("user_id")
+    if not user_id:
+        return RedirectResponse(url="/login", status_code=302)
+    
+    return templates.TemplateResponse("create_request.html",{"request":request, "courses": COURSES, "payments": PAYMNETS})
+
+
+@app.post("/create_request")
+def get_create_request(request: Request,
+                course: str = Form(...),
+                payment: str = Form(...),
+                date: str = Form(...)):
+    user_id = request.session.get("user_id")
+    if not user_id:
+        return RedirectResponse(url="/login", status_code=302)
+    
+    with get_db() as conn:
+        conn.execute("insert into requests (course_name,payment_method,date_start,user_id) values (?,?,?,?)", (course,payment,date,user_id))
+    
+    return RedirectResponse(url="/profile", status_code=302)
+
+@app.post("/add_review")
+def post_profile(request: Request,
+                 request_id: str = Form(...),
+                 review: str = Form(...)):
+    user_id = request.session.get("user_id")
+    if not user_id:
+        return RedirectResponse(url="/login", status_code=302)
+    
+
+    with get_db() as conn:
+        cur= conn.execute("select * from requests where id =  ?", (request_id,)).fetchone()
+
+    if cur:
+        with get_db() as conn:
+            conn.execute("update requests set review = ? where id = ?", (review,request_id))
+    
+    return RedirectResponse(url="/profile", status_code=302)
+
+@app.post("/admin/change_status")
+def post_admin_change_status(request: Request,
+                 request_id: str = Form(...),
+                 status: str = Form(...)):
+    user_id = request.session.get("admin")
+    if not user_id:
+        return RedirectResponse(url="/login", status_code=302)
+    
+    with get_db() as conn:
+        conn.execute("update requests set status = ? where id = ?", (status,request_id))
+    
+    return RedirectResponse(url="/admin", status_code=302)
