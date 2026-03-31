b316f942d7ee131bbf73b2145efcb214f06138d2
Frese Fea <fresefae@gmail.com>
2026-03-31 21:10:37 +0300

    Попытка 13

--- Popitka13/main.py
+++ Popitka13/main.py
@@ -14,7 +14,8 @@ def get_db():
     conn = sqlite3.connect("database.db")
     conn.row_factory = sqlite3.Row
     return conn
-
+ 
+ 
 def init_db():
     with get_db() as conn:
         conn.executescript("""
